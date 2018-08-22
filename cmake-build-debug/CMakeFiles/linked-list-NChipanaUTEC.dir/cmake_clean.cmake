03562565075703ecbe9ea7408771b4d3c341d724
Chip <nicolas.chipana@utec.edu.pe>
2018-08-21 22:27:20 -0500

    First Push

---
+++ cmake-build-debug/CMakeFiles/linked-list-NChipanaUTEC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linked-list-NChipanaUTEC.dir/main.cpp.o"
+  "linked-list-NChipanaUTEC.pdb"
+  "linked-list-NChipanaUTEC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linked-list-NChipanaUTEC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
